c43df50ab205c70597e42f931ef351e2731bf848
unknown <robert.tanksley@gmail.com>
2020-02-12 19:03:12 -0600

    matched mvp

--- js/index.js
+++ js/index.js
@@ -39,4 +39,45 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let ctaImg = document.getElementById("cta-img");
+let middleImg = document.getElementById("middle-img");
+ctaImg.src = 'img/header-img.png';
+middleImg.src = 'img/mid-page-accent.jpg';
+
+let navArray = document.getElementsByTagName('a');
+navArray[0].textContent = 'Services' ;
+navArray[1].textContent = 'Product' ;
+navArray[2].textContent = 'Vision'; 
+navArray[3].textContent = 'Features';
+navArray[4].textContent = 'About'; 
+navArray[5].textContent = 'Contact'; 
+// console.log(navArray)
+
+let ctaTextH1 = document.getElementsByTagName('h1');
+// console.log(ctaTextH1);
+ctaTextH1[0].innerHTML ='DOM<BR> IS<BR> AWESOME';
+let ctaButtonText = document.getElementsByTagName('button');
+ctaButtonText[0].textContent ='Get Started';
+
+let sectionHeader = document.getElementsByTagName('h4');
+// console.log(sectionHeader);
+sectionHeader[0].textContent = 'Features' ;
+sectionHeader[1].textContent = 'About' ;
+sectionHeader[2].textContent = 'Services'; 
+sectionHeader[3].textContent = 'Product';
+sectionHeader[4].textContent = 'Vision'; 
+sectionHeader[5].textContent = 'Contact'; 
+
+let sectionText = document.getElementsByTagName('p');
+console.log(sectionText)
+sectionText[0].textContent ='Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+sectionText[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+sectionText[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+sectionText[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+sectionText[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+sectionText[5].innerHTML = '123 Way 456 Street<br> Somewhere, USA';
+sectionText[6].textContent = '1 (888) 888-8888';
+sectionText[7].textContent = 'sales@greatidea.io';
+sectionText[8].textContent = 'Copyright Great Idea! 2018';
